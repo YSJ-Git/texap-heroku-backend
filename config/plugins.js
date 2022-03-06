79c8967439f102ef878cc19fe8a2c75d0a4b44e5
YSJ-Git <100123808+YSJ-Git@users.noreply.github.com>
2022-03-06 18:14:47 +0900

    Create plugins.js

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    upload: {
+        provider: "cloudinary",
+        providerOptions: {
+            cloud_name: "applotnwjd",
+            api_key: "131469728462133",
+            api_secret: "K_K8XkXKSXJcqxzy8wz2tUhzpEE",
+        },
+    },
+});
